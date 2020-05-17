0cccca525ea85b034734bece6f14081d4c454a5a
Manak Pandey <manakpandey@gmail.com>
2020-05-17 14:36:25 +0530

    converted to functional component

--- client/src/components/forms/AddProduct.jsx
+++ client/src/components/forms/AddProduct.jsx
@@ -1,72 +1,39 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
 
+const AddProduct = () => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
 
-class ContactForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      pid:'',
-    };
-
-    this.handleChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+  const handleSubmit = e =>{
+    e.preventDefault()
   }
 
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
-    
-    this.setState({
-      [pid]: value
-    });
-    console.log('Change detected. State updated' + name + ' = ' + value);
-  }
+  return (
+    <div>
+      <form onSubmit={e=>handleSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="text"
+            name="name"
+            onChange={t=>setName(t.target.value)}
+            className="form-control"
+            placeholder="Product Name"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            name="price"
+            type="int"
+            onChange={t=>setPrice(t.target.value)}
+            className="form-control"
+            placeholder="Product Price"
+          />
+        </div>
 
-  handleSubmit(event) {
-    alert('Product Added: ' + this.state.pid + ' // ' + this.state.name);
-    event.preventDefault();
-  }
+        <input type="submit" value="Add Product" className="btn btn-primary" />
+      </form>
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit} >
-          <div className="form-group">
-            <input type="int" name="pid" value={this.state.pid} onChange={this.handleChange} className="form-control" id="pidImput" placeholder="Product ID" />
-          </div>
-          <div className="form-group">
-            {/* <label for="emailInput">Name</label> */}
-            <input name="name" type="text" value={this.state.name} onChange={this.handleChange} className="form-control" id="nameImput" placeholder="Product Name" />
-          </div>
-          
-          <input type="submit" value="Add Product" className="btn btn-primary" />
-        </form>
-      </div>
-    )
-  }
-}
-
-class MainTitle extends React.Component {
-  render(){
-    return(
-      <h1>Add Product</h1>
-    )
-  }
-}
-
-class App extends React.Component {
-  render(){
-    return(
-      <div>
-        <MainTitle/>
-        <ContactForm/>
-      </div>
-    )
-  }
-}
-
-
-export default App;
+export default AddProduct;
