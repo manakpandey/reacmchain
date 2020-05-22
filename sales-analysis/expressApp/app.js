52b0ed7f2ccafabb0ea601b48dfbd81c8b047c11
Manak Pandey <manakpandey@gmail.com>
2020-05-23 00:30:00 +0530

    connect node to python

--- sales-analysis/expressApp/app.js
+++ sales-analysis/expressApp/app.js
@@ -1,39 +1,32 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const { PythonShell } = require("python-shell");
+const bodyParser = require("body-parser");
 
-var indexRouter = require('./routes/index');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+const port = 4000;
 
-var app = express();
+app.get("/getDemand", (req, res) => {
+  const pyshell = new PythonShell("script.py");
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/getDemand', indexRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+  pyshell.on("message", function (message) {
+    res.send(message);
+  });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.post("/retrain", (req, res) => {
+  const pyshell = new PythonShell("script1.py");
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  console.log(req.body);
+
+  pyshell.send(req.body);
+
+  pyshell.on("message", function (message) {
+    res.send(message);
+  });
 });
 
-module.exports = app;
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
